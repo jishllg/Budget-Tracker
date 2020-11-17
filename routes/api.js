98f042a111367717e0736bd098262e1f1a00a31e
Jishllg Laptop <jishllg@gmail.com>
2020-11-17 12:52:24 -0600

    Adjusted routes for service worker compatability.

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
+// router.post("/api/transaction", ({body}, res) => {
+//   Transaction.create(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(404).json(err);
+//     });
+// });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/new", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -21,7 +21,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
-router.get("/api/transaction", (req, res) => {
+router.get("/api/transaction/get", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
